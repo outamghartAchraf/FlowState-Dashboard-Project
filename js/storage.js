40fa7884ffb84b5528d44c9a0351acb40f4e09bb
outamghartAchraf <outamghart2000@gmail.com>
2026-02-12 14:06:12 +0100

    function for loadTasks

--- js/storage.js
+++ js/storage.js
@@ -3,3 +3,7 @@ export function saveTasks(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+export function loadTasks() {
+  const data = localStorage.getItem('tasks');
+  return data ? JSON.parse(data) : [];
+}
